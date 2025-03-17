7d9784a55cd323c2501b5448035d41e5fd6cac06
MymoMg <mmguil68@gmail.com>
2025-03-17 11:44:14 +0100

    Add Hero Component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import styles from "../styles/Hero.module.css"
+
+export default function Hero() {
+    return (
+        <div className={styles.homePage}>
+        <h1>🎬 Bienvenue sur Moviz ! 🍿</h1>
+        <p>Avec Moviz, crée ta propre liste de films, note ceux que tu as vus et découvre de nouveaux films à regarder. Plus besoin de te souvenir de tous les titres, tout est stocké en un seul endroit.</p>
+        <ul>
+            <h2>Fonctionnalités principales :</h2>
+            <li>Ajoute et organise tes films</li>
+            <li>Note et laisse des avis</li>
+            <li>Découvre de nouveaux films à voir</li>
+        </ul>
+        <p>Simple, rapide et efficace. Moviz t’aide à ne jamais manquer un bon film !</p>
+    </div>
+    )
+};
